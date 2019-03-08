332cb82876139377f8b099dfe47c9a41a8351260
nunes-a <amsjnunes@live.com.pt>
2019-03-08 16:14:54 +0100

    set up adeus

--- resources/views/adeus.blade.php
+++ resources/views/adeus.blade.php
@@ -70,7 +70,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    "Adeus" en portugais
+                    Adeus en portugais
                 </div>
             </div>
         </div>
