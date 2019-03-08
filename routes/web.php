bcc7e40d33be9b14690a09cbe6c0427fef94b811
Hamza Serhane <hamza42700@gmail.com>
2019-03-08 16:16:20 +0100

    adeus page

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
  Route::get('/adios', function () {
         return view('adios');
  });
+
+ 
+     Route::get('/adeus', function () {
+         return view('adeus');
+     });
