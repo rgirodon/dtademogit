c7bb35ca6b6376fc4ef987137c200c8524b4c662
Hamza Serhane <hamza42700@gmail.com>
2019-03-08 15:19:58 +0100

    adios page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,7 +70,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    DTA Demo project
+                   DTA DEMO GIT
                 </div>
             </div>
         </div>
